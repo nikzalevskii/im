2fd084138c4d659258f3ca46c4b4584b0ffae98a
Nikita Zalevskii <nzalevskii@yandex.ru>
2024-12-16 16:19:58 +0100

    docs folder 1.006

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // api: 'https://zolich.ru/api/',
   // serverStaticPath: 'https://zolich.ru/images/products/',
-  api: 'https://im-cfjf.onrender/api/',
-  serverStaticPath: 'https://im-cfjf.onrender/images/products/',
+  api: 'https://im-cfjf.onrender.com/api/',
+  serverStaticPath: 'https://im-cfjf.onrender.com/images/products/',
 };
