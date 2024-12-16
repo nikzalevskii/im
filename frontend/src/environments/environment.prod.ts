f5bb34d95ae6b151a908c831ced7994f323ea1e3
Nikita Zalevskii <nzalevskii@yandex.ru>
2024-12-16 13:07:37 +0100

    update environment v1.001

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  api: 'https://zolich.ru/api/',
-  serverStaticPath: 'https://zolich.ru/images/products/',
+  // api: 'https://zolich.ru/api/',
+  // serverStaticPath: 'https://zolich.ru/images/products/',
+  api: 'https://im-cfjf.onrender/api/',
+  serverStaticPath: 'https://im-cfjf.onrender/images/products/',
 };
